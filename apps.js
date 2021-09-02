d8d9624d2c86b0b0fb7ca0aa2863f1b308f37fa8
yashpatel1797 <yes.g.patel@gmail.com>
2021-09-02 18:22:01 +0530

    initial commit

---
+++ apps.js
@@ -0,0 +1,33 @@
+var initialPrice = document.querySelector("#initial-price");
+var stockQuantity = document.querySelector("#stock-quantity");
+var currentPrice = document.querySelector("#current-price");
+var btnSubmit = document.querySelector("#btn-submit");
+var output = document.querySelector("#output");
+
+
+
+function submitHandler(){
+    calculateProfitAndLoss(Number(initialPrice.value), Number(stockQuantity.value), Number(currentPrice.value));
+}
+
+function calculateProfitAndLoss(initial, quantity, current){
+    if(initial > current){
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+        showMessage(`Loss is ${loss} and the percent is ${lossPercentage}`, "Red");
+    } else if(initial < current){
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit/initial) * 100;
+        showMessage(`Profit is ${profit} and the percent is ${profitPercentage}`, "Green");
+    } else {
+        showMessage(`No Profit or Loss`, "black");
+    }
+}
+
+function showMessage(message, color){
+    output.innerText = message;
+    output.style.color = color;
+
+}
+
+btnSubmit.addEventListener("click", submitHandler);
